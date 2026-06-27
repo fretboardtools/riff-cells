b5520be4ad5a51890c484815c596d1d469a6ad76
fretboardtools <graham.tippett@gmail.com>
2026-06-26 23:11:24 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import LandingPage from "./LandingPage.jsx";
+import RiffCells from "./RiffCells.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LandingPage />
+    <RiffCells />
   </React.StrictMode>
 );
